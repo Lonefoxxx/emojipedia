901fdccfc2b145f1e1b77dbb8122617d925da73f
Aravind J <61082145+Lonefoxxx@users.noreply.github.com>
2021-02-08 22:41:38 +0530

    Add files via upload

---
+++ emojipedia.js
@@ -0,0 +1,25 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "🎈",
+    name: "The Balloon",
+    meaning:
+      "Sometimes when you overwhelmed and lose words to express how you feel you send this you express your LOVE"
+  },
+  {
+    id: 2,
+    emoji: "🍃",
+    name: "Leaf on Breeze",
+    meaning:
+      "You having a busy day. Not a soul around to disturb :p You send this to make your girl feel the breeze of your LOVE"
+  },
+  {
+    id: 3,
+    emoji: "🐣",
+    name: "Kutty Chicken",
+    meaning:
+      "Leee meee hugggyyyy. Kutty chicken to express your LOVE."
+  }
+];
+
+export default emojipedia;
